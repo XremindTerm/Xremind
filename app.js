1ba0d54463d964e25019d3735b6aad07f4a8efd5
SFSM <isworkinghard@163.com>
2016-07-26 20:43:24 +0800

    add express-session

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session=require('express-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -26,7 +27,12 @@ nunjucks.configure('views', {
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser("An"));
+app.use(session({
+  secret:'an',
+  resave:false,
+  saveUninitialized:true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
