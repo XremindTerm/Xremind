2e4b702988755a004a9e98892e281399b445199a
SFSM <isworkinghard@163.com>
2016-07-26 19:56:02 +0800

    create table users

---
+++ sql/README.md
@@ -0,0 +1,33 @@
+### 数据库设计
+1.reminders表
+
+字段名	|类型		|注释
+:------:|:---------:|--------------------------------
+id		|int(11)	|索引(Key)
+uid		|int(11)	|用户编号(FKey)
+data	|text		|存放提醒项目的参数(JSON格式)
+target	|varchar(15)|下次提醒时间戳
+interval|varchar(15)|下次提醒间隔
+status	|varchar(10)|标记提醒项目状态：wait\|action\|done
+
+```javascript  
+    //使用逻辑介绍
+    var init =new Date().getTime(); //项目初次建立的时间戳
+    var interval=5*60*1000;         //下次提醒的时间间隔(ms)
+    var target=init+interval;       //下次提醒命中的时间戳
+    setInterval(function(){
+        if(new Date().getTime()>=target){  //若当前时间该提醒了
+            console.log('action');
+            interval=newInterval();       //更新下次更新时间
+            target+=interval;		      //更新下次命中时间
+        }
+    },1000);
+```
+
+2.users表
+字段名 |类型     |注释
+:------:|:---------:|--------------------------------
+id      |int(11)    |索引(Key)
+usermail|varchar(30)|用户邮箱
+password|varchar(35)|用户密码(MD5)
+nickname|varchar(30)|用户昵称
---
+++ sql/users.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 2016-07-26 11:55:26
+-- 服务器版本： 5.7.11
+-- PHP Version: 5.6.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `xremind`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL COMMENT '用户编号',
+  `usermail` varchar(30) NOT NULL COMMENT '用户邮箱',
+  `nickname` varchar(20) NOT NULL COMMENT '用户昵称',
+  `password` varchar(35) NOT NULL COMMENT '用户密码'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='用户表';
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在导出的表使用AUTO_INCREMENT
+--
+
+--
+-- 使用表AUTO_INCREMENT `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户编号', AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
