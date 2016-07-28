0ee45d6e6e0c9165225d133605a74ac5a26f84e1
SFSM <isworkinghard@163.com>
2016-07-29 01:28:47 +0800

    add user's auth check

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+//User Auth Check
+router.all('*', function(req, res, next) {
+	var reg=/^\/user(\/|\/login|\/register)?$/;
+	if(!req.session.userinfo&&!reg.test(req.originalUrl)){
+		if(req.is('json')){
+			res.jsonp({state:'err',detail:'please login first'});
+		}else{
+			res.redirect('/user');
+		}
+	}else next();
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	if(req.session.userinfo){
