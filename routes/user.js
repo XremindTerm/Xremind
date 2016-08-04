92ce77492afc9e9fa91e6d2951ab84c39ce79779
SFSM565826960 <sfsm565826960@gmail.com>
2016-08-04 15:21:23 +0800

    add edit.html and index.html; fix user.js-Out._opt

--- routes/user.js
+++ routes/user.js
@@ -18,7 +18,7 @@ function Out(req,res,defaultView,defaultViewOpt){
 		_req:req,
 		_res:res,
 		_view:defaultView,
-		_opt:defaultViewOpt,//渲染所需的默认参数，参数可被echo中的obj中覆盖
+		_opt:defaultViewOpt||{},//渲染所需的默认参数，参数可被echo中的obj中覆盖
 		echo:function(obj,view){
 			if(this._req.is('json')){
 				this._res.jsonp(obj);
